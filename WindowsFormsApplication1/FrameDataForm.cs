828e355d220b08b32dccb415533dd7c19889b17c
icvav@yahoo.es <icvav@yahoo.es>
2015-04-13 20:16:33 +0200

    kj

--- WindowsFormsApplication1/FrameDataForm.cs
+++ WindowsFormsApplication1/FrameDataForm.cs
@@ -73,7 +73,7 @@ namespace WindowsFormsApplication1
             this.displayGestureCount.Text = frame.Gestures().Count.ToString();
             this.displayImageCount.Text = frame.Images.Count.ToString();
 
-            //hola2
+            //hola25
 
             this.aux.Text = "Frame id4: " + frame.Id
                      + ", timestamp: " + frame.Timestamp
