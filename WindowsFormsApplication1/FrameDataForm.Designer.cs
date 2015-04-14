d990616dd61bf2c26d6134c37c2f78e3f069768a
icvav@yahoo.es <icvav@yahoo.es>
2015-04-14 20:43:47 +0200

    hui

--- WindowsFormsApplication1/FrameDataForm.Designer.cs
+++ WindowsFormsApplication1/FrameDataForm.Designer.cs
@@ -43,11 +43,30 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.d1AngProxInt = new System.Windows.Forms.Label();
+            this.d1AngIntDist = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.d1AngMetProx = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.d2AngMetProx = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.d2AngIntDist = new System.Windows.Forms.Label();
+            this.d2AngProxInt = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(703, 179);
+            this.button1.Location = new System.Drawing.Point(649, 26);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -112,7 +131,7 @@
             // aux
             // 
             this.aux.AutoSize = true;
-            this.aux.Location = new System.Drawing.Point(86, 238);
+            this.aux.Location = new System.Drawing.Point(86, 123);
             this.aux.Name = "aux";
             this.aux.Size = new System.Drawing.Size(35, 13);
             this.aux.TabIndex = 8;
@@ -175,17 +194,177 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(13, 238);
+            this.label7.Location = new System.Drawing.Point(15, 123);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(28, 13);
             this.label7.TabIndex = 15;
             this.label7.Text = "Aux:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Location = new System.Drawing.Point(16, 139);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(434, 399);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mano 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(456, 139);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 399);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mano 2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.d1AngMetProx);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.d1AngIntDist);
+            this.groupBox3.Controls.Add(this.d1AngProxInt);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Location = new System.Drawing.Point(6, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(422, 100);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Dedo 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Ang. Prox/Interm:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 53);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Ang. Interm/Distal:";
+            // 
+            // d1AngProxInt
+            // 
+            this.d1AngProxInt.AutoSize = true;
+            this.d1AngProxInt.Location = new System.Drawing.Point(106, 36);
+            this.d1AngProxInt.Name = "d1AngProxInt";
+            this.d1AngProxInt.Size = new System.Drawing.Size(41, 13);
+            this.d1AngProxInt.TabIndex = 2;
+            this.d1AngProxInt.Text = "label10";
+            // 
+            // d1AngIntDist
+            // 
+            this.d1AngIntDist.AutoSize = true;
+            this.d1AngIntDist.Location = new System.Drawing.Point(106, 52);
+            this.d1AngIntDist.Name = "d1AngIntDist";
+            this.d1AngIntDist.Size = new System.Drawing.Size(41, 13);
+            this.d1AngIntDist.TabIndex = 3;
+            this.d1AngIntDist.Text = "label10";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 16);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Ang. Met/Prox:";
+            // 
+            // d1AngMetProx
+            // 
+            this.d1AngMetProx.AutoSize = true;
+            this.d1AngMetProx.Location = new System.Drawing.Point(109, 15);
+            this.d1AngMetProx.Name = "d1AngMetProx";
+            this.d1AngMetProx.Size = new System.Drawing.Size(41, 13);
+            this.d1AngMetProx.TabIndex = 5;
+            this.d1AngMetProx.Text = "label11";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.d2AngMetProx);
+            this.groupBox4.Controls.Add(this.label12);
+            this.groupBox4.Controls.Add(this.d2AngIntDist);
+            this.groupBox4.Controls.Add(this.d2AngProxInt);
+            this.groupBox4.Controls.Add(this.label15);
+            this.groupBox4.Controls.Add(this.label16);
+            this.groupBox4.Location = new System.Drawing.Point(6, 125);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(422, 100);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Dedo 1";
+            // 
+            // d2AngMetProx
+            // 
+            this.d2AngMetProx.AutoSize = true;
+            this.d2AngMetProx.Location = new System.Drawing.Point(109, 15);
+            this.d2AngMetProx.Name = "d2AngMetProx";
+            this.d2AngMetProx.Size = new System.Drawing.Size(41, 13);
+            this.d2AngMetProx.TabIndex = 5;
+            this.d2AngMetProx.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 16);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(79, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Ang. Met/Prox:";
+            // 
+            // d2AngIntDist
+            // 
+            this.d2AngIntDist.AutoSize = true;
+            this.d2AngIntDist.Location = new System.Drawing.Point(106, 52);
+            this.d2AngIntDist.Name = "d2AngIntDist";
+            this.d2AngIntDist.Size = new System.Drawing.Size(41, 13);
+            this.d2AngIntDist.TabIndex = 3;
+            this.d2AngIntDist.Text = "label10";
+            // 
+            // d2AngProxInt
+            // 
+            this.d2AngProxInt.AutoSize = true;
+            this.d2AngProxInt.Location = new System.Drawing.Point(106, 36);
+            this.d2AngProxInt.Name = "d2AngProxInt";
+            this.d2AngProxInt.Size = new System.Drawing.Size(41, 13);
+            this.d2AngProxInt.TabIndex = 2;
+            this.d2AngProxInt.Text = "label10";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(6, 53);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(95, 13);
+            this.label15.TabIndex = 1;
+            this.label15.Text = "Ang. Interm/Distal:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(6, 36);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(90, 13);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "Ang. Prox/Interm:";
             // 
             // FrameDataForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(874, 550);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -205,6 +384,11 @@
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,6 +411,22 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label d1AngIntDist;
+        private System.Windows.Forms.Label d1AngProxInt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label d1AngMetProx;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label d2AngMetProx;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label d2AngIntDist;
+        private System.Windows.Forms.Label d2AngProxInt;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
     }
 }
 
--- WindowsFormsApplication1/FrameDataForm.cs
+++ WindowsFormsApplication1/FrameDataForm.cs
@@ -75,13 +75,119 @@ namespace WindowsFormsApplication1
 
             //hola25
 
-            this.aux.Text = "Frame id4: " + frame.Id
-                     + ", timestamp: " + frame.Timestamp
-                     + ", hands: " + frame.Hands.Count
-                     + ", fingers: " + frame.Fingers.Count
-                     + ", tools: " + frame.Tools.Count
-                     + ", gestures: " + frame.Gestures().Count;
+            //this.aux.Text = "Frame id4: " + frame.Id
+            //         + ", timestamp: " + frame.Timestamp
+            //         + ", hands: " + frame.Hands.Count
+            //         + ", fingers: " + frame.Fingers.Count
+            //         + ", tools: " + frame.Tools.Count
+            //         + ", gestures: " + frame.Gestures().Count;
 
+
+            Vector vectorHueso1 = frame.Hands[0].Fingers[1].Bone(Bone.BoneType.TYPE_PROXIMAL).Direction;
+            Vector vectorHueso2 = frame.Hands[0].Fingers[2].Bone(Bone.BoneType.TYPE_PROXIMAL).Direction;
+
+            float dotProduct = vectorHueso1.Dot(vectorHueso2);
+
+            double angulo = Math.Acos(dotProduct);
+
+           // SafeWriteLine("angulo normal: " + angulo);
+
+            int anguloEntero = Convert.ToInt32(angulo * 100);
+
+            this.aux.Text = angulo.ToString();
+
+           // SafeWriteLine("angulo entero: " + anguloEntero);
+
+
+            #region cargarMano1
+            Hand mano1 = frame.Hands[0];
+
+            Finger mano1Dedo1 = mano1.Fingers[0];
+            Finger mano1Dedo2 = mano1.Fingers[1];
+            Finger mano1Dedo3 = mano1.Fingers[2];
+            Finger mano1Dedo4 = mano1.Fingers[3];
+            Finger mano1Dedo5 = mano1.Fingers[4];
+
+            Bone mano1Dedo1Metacarpo = mano1Dedo1.Bone(Bone.BoneType.TYPE_METACARPAL);
+            Bone mano1Dedo1Proximal = mano1Dedo1.Bone(Bone.BoneType.TYPE_PROXIMAL);
+            Bone mano1Dedo1Intermedio = mano1Dedo1.Bone(Bone.BoneType.TYPE_INTERMEDIATE);
+            Bone mano1Dedo1Distal = mano1Dedo1.Bone(Bone.BoneType.TYPE_DISTAL);
+
+            Bone mano1Dedo2Metacarpo = mano1Dedo2.Bone(Bone.BoneType.TYPE_METACARPAL);
+            Bone mano1Dedo2Proximal = mano1Dedo2.Bone(Bone.BoneType.TYPE_PROXIMAL);
+            Bone mano1Dedo2Intermedio = mano1Dedo2.Bone(Bone.BoneType.TYPE_INTERMEDIATE);
+            Bone mano1Dedo2Distal = mano1Dedo2.Bone(Bone.BoneType.TYPE_DISTAL);
+
+            Bone mano1Dedo3Metacarpo = mano1Dedo3.Bone(Bone.BoneType.TYPE_METACARPAL);
+            Bone mano1Dedo3Proximal = mano1Dedo3.Bone(Bone.BoneType.TYPE_PROXIMAL);
+            Bone mano1Dedo3Intermedio = mano1Dedo3.Bone(Bone.BoneType.TYPE_INTERMEDIATE);
+            Bone mano1Dedo3Distal = mano1Dedo3.Bone(Bone.BoneType.TYPE_DISTAL);
+
+            Bone mano1Dedo4Metacarpo = mano1Dedo4.Bone(Bone.BoneType.TYPE_METACARPAL);
+            Bone mano1Dedo4Proximal = mano1Dedo4.Bone(Bone.BoneType.TYPE_PROXIMAL);
+            Bone mano1Dedo4Intermedio = mano1Dedo4.Bone(Bone.BoneType.TYPE_INTERMEDIATE);
+            Bone mano1Dedo4Distal = mano1Dedo4.Bone(Bone.BoneType.TYPE_DISTAL);
+
+            Bone mano1Dedo5Metacarpo = mano1Dedo5.Bone(Bone.BoneType.TYPE_METACARPAL);
+            Bone mano1Dedo5Proximal = mano1Dedo5.Bone(Bone.BoneType.TYPE_PROXIMAL);
+            Bone mano1Dedo5Intermedio = mano1Dedo5.Bone(Bone.BoneType.TYPE_INTERMEDIATE);
+            Bone mano1Dedo5Distal = mano1Dedo5.Bone(Bone.BoneType.TYPE_DISTAL);
+
+            Vector mano1Dedo1MetacarpoDireccion = mano1Dedo1Metacarpo.Direction;
+            Vector mano1Dedo1ProximalDireccion = mano1Dedo1Proximal.Direction;
+            Vector mano1Dedo1IntermedioDireccion = mano1Dedo1Intermedio.Direction;
+            Vector mano1Dedo1DistalDireccion = mano1Dedo1Distal.Direction;
+
+            Vector mano1Dedo2MetacarpoDireccion = mano1Dedo2Metacarpo.Direction;
+            Vector mano1Dedo2ProximalDireccion = mano1Dedo2Proximal.Direction;
+            Vector mano1Dedo2IntermedioDireccion = mano1Dedo2Intermedio.Direction;
+            Vector mano1Dedo2DistalDireccion = mano1Dedo2Distal.Direction;
+
+            Vector mano1Dedo3MetacarpoDireccion = mano1Dedo3Metacarpo.Direction;
+            Vector mano1Dedo3ProximalDireccion = mano1Dedo3Proximal.Direction;
+            Vector mano1Dedo3IntermedioDireccion = mano1Dedo3Intermedio.Direction;
+            Vector mano1Dedo3DistalDireccion = mano1Dedo3Distal.Direction;
+
+            Vector mano1Dedo4MetacarpoDireccion = mano1Dedo3Metacarpo.Direction;
+            Vector mano1Dedo4ProximalDireccion = mano1Dedo3Proximal.Direction;
+            Vector mano1Dedo4IntermedioDireccion = mano1Dedo3Intermedio.Direction;
+            Vector mano1Dedo4DistalDireccion = mano1Dedo3Distal.Direction;
+
+            Vector mano1Dedo5MetacarpoDireccion = mano1Dedo4Metacarpo.Direction;
+            Vector mano1Dedo5ProximalDireccion = mano1Dedo4Proximal.Direction;
+            Vector mano1Dedo5IntermedioDireccion = mano1Dedo4Intermedio.Direction;
+            Vector mano1Dedo5DistalDireccion = mano1Dedo4Distal.Direction; 
+
+            #endregion
+
+
+            this.d1AngMetProx.Text = calcularAngulo(mano1Dedo1MetacarpoDireccion, mano1Dedo1ProximalDireccion);
+            this.d1AngProxInt.Text = calcularAngulo(mano1Dedo1ProximalDireccion, mano1Dedo1IntermedioDireccion);
+            this.d1AngIntDist.Text = calcularAngulo(mano1Dedo1IntermedioDireccion, mano1Dedo1DistalDireccion);
+
+
+            this.d2AngMetProx.Text = calcularAngulo(mano1Dedo2MetacarpoDireccion, mano1Dedo2ProximalDireccion);
+            this.d2AngProxInt.Text = calcularAngulo(mano1Dedo2ProximalDireccion, mano1Dedo2IntermedioDireccion);
+            this.d2AngIntDist.Text = calcularAngulo(mano1Dedo2IntermedioDireccion, mano1Dedo2DistalDireccion);
+
+        }
+
+        private string calcularAngulo(Vector direccion1, Vector direccion2) {
+            float dotProduct = direccion1.Dot(direccion2);
+            
+            //Arcocoseno es una función que solo está definida entre -1 y 1. 
+            //A veces el producto vectorial es un poco superior a 1 y casca
+            //TODO: revisar
+            if (dotProduct > 1)
+                dotProduct = 1;
+            if (dotProduct < -1)
+                dotProduct = -1;
+
+            this.aux.Text = dotProduct.ToString();
+
+            double angulo = Math.Acos(dotProduct);
+            int anguloEntero = Convert.ToInt32(angulo * 100);
+            return anguloEntero.ToString();
         }
 
         //protected override void Dispose(bool disposing)
